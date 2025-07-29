b679ad19031489c96eebdede9371afccac1c5abb
duxstein <sportswatch001@gmail.com>
2025-07-29 23:05:19 +0530

    feat: add module initialization files for gui and speech packages

---
+++ src/gui/__init__.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python3
+"""
+GUI Module
+"""
+
+from .main_window import NexaMainWindow
---
+++ src/speech/__init__.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+"""
+Speech Module
+"""
+
+from .voice_recognition import VoiceRecognition
+from .text_to_speech import TextToSpeech
